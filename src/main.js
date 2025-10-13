6a25dbdbe9c2f90eb39c88f50436cce8374c8364
Byte-nana <nanadevelop44@gmail.com>
2025-10-13 16:35:59 +0900

    feat:add item on the to-do list

---
+++ src/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//add items
+
+const form = document.querySelector('.footer__form');
+const ul = document.querySelector('.main__items');
+const input = document.querySelector('#form__input');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let text = input.value.trim();
+
+  if (text == '') {
+    input.value = '';
+    return;
+  }
+  createItem(text);
+  input.value = '';
+});
+
+function createItem(text) {
+  const li = document.createElement('li');
+  li.setAttribute('class', 'item__row');
+  li.innerHTML = `
+<label for="item__checkbox" class="item__label">
+    <input id="item__checkbox" type="checkbox"/>
+    <span class="item__text">${text}</span>
+</label>
+<button class="item__btn" type="button" aria-label="button for deleting the to do list">
+    <i class="fa-solid fa-trash"></i>
+</button>
+`;
+  ul.appendChild(li);
+}
