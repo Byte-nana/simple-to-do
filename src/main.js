cd63223d0f3433865344f7a4905de8295fd23644
Byte-nana <nanadevelop44@gmail.com>
2025-10-13 21:33:28 +0900

    feat:delete list item

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ const form = document.querySelector('.footer__form');
 const ul = document.querySelector('.main__items');
 const input = document.querySelector('#form__input');
 
+let id = 0;
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   let text = input.value.trim();
@@ -15,20 +16,24 @@ form.addEventListener('submit', (e) => {
     return;
   }
   createItem(text);
+  id++;
+
   input.value = '';
 });
 
 function createItem(text) {
   const li = document.createElement('li');
   li.setAttribute('class', 'item__row');
+  li.setAttribute('data-id', id);
   li.innerHTML = `
-<label for="item__checkbox" class="item__label">
-    <input id="item__checkbox" type="checkbox"/>
-    <span class="item__text">${text}</span>
-</label>
-<button class="item__btn" type="button" aria-label="button for deleting the to do list">
-    <i class="fa-solid fa-trash"></i>
-</button>
-`;
+    <label for="item__checkbox" class="item__label">
+      <input id="item__checkbox" type="checkbox"/>
+      <span class="item__text">${text}</span>
+    </label>
+    <button class="item__btn" type="button" aria-label="button for deleting the to do list">
+      <i class="fa-solid fa-trash" data-id="${id}"></i>
+    </button>
+  `;
+
   ul.appendChild(li);
 }
